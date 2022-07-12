f05865fe21afcbea5d65bab78a056796cd7ee683
cus <2417837761@qq.com>
2022-07-12 11:44:15 +0800

    first commit

---
+++ src/main/java/com/cus/example/App.java
@@ -0,0 +1,18 @@
+ package com.cus.example;
+
+/**
+ * @Description: TODO
+ * @Author: wangyu
+ * @Date: 2022/7/12 11:27
+ * @PackageName: com.cus.example
+ * @ClassName: App
+ * @Contact: 2417837761@qq.com
+ * @Version: 2022.0.1
+ */
+public class App {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
